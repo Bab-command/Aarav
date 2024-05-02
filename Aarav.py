1c9acbced91b3dd8b988f289a9e76654d494fd8c
Bab-command <168257666+Bab-command@users.noreply.github.com>
2024-05-02 14:05:08 +0500

    Update Aarav.py

--- Aarav.py
+++ Aarav.py
@@ -1,1 +1,25 @@
-import bab1_enc
+#####apni 
+ #coding=utf-8
+import os, sys, platform
+ 
+os.system('git pull')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('git pull')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+        import bab64_enc
+    else:
+        import bab64_enc
+ 
+elif bit == '32bit':
+    
+        import bab1_enc
+    else:
+        import bab1_enc
